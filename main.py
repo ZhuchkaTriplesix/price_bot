677b9d2af894a5a09726ba09d4b38478b70e3762
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-02 11:08:28 +0600

    feat(Cases function): Add Cases function that checking cases list price

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ with open("token.txt", "r") as TOKEN:
     bot_token = TOKEN.readline()
 
 bot = telebot.TeleBot(bot_token)
+cases_list = []
 
 
 @bot.message_handler(commands=["start"])
@@ -14,11 +15,16 @@ def start(message):
     bot.send_message(message.chat.id, 'Hello', reply_markup=keyboard)
 
 
-
-
-
-
-
+@bot.message_handler(content_types=["text"])
+def cases(message):
+    if message.text in ("cases", "Cases"):
+        for case in cases_list:
+            case = case.lower().capitalize() + " Case"
+            case_price = sm.get_item(730, case, currency='RUB')
+            if case_price['success'] is False:
+                bot.send_message(message.chat.id, f"Can't find item {case}")
+            else:
+                bot.send_message(message.chat.id, case + ": " + str(case_price["lowest_price"]))
 
 
 bot.polling(none_stop=True)
