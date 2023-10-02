82760aff7d29e68eceabc62f919e8ded490fe058
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-02 10:44:58 +0600

    feat(Buttons): Add 2 buttons in telegram bot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import steammarket as sm
 import telebot
-import token
 
 with open("token.txt", "r") as TOKEN:
     bot_token = TOKEN.readline()
@@ -8,4 +7,18 @@ with open("token.txt", "r") as TOKEN:
 bot = telebot.TeleBot(bot_token)
 
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row("Cases", "Item list")
+    bot.send_message(message.chat.id, 'Hello', reply_markup=keyboard)
 
+
+
+
+
+
+
+
+
+bot.polling(none_stop=True)
