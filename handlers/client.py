af5752f953034615fac9ca0780da8c7bfbfbf4bd
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-04 17:48:25 +0600

    feat(States):Added States to editing notification time

--- handlers/client.py
+++ handlers/client.py
@@ -1,9 +1,11 @@
 from aiogram import F
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, CallbackQuery
 import steammarket as sm
 import json_support
 import keyboards as kb
 from aiogram import Router
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 cases_list = []
@@ -12,34 +14,47 @@ json_data = "user_list.json"
 push_data = "users_time.json"
 
 
+class NotificationOrder(StatesGroup):
+    choosing_notification = State()
+    choosing_add_notification = State()
+    response_notification_state = State()
+    time_add_notification_state = State()
+
+
 @router.message(F.text == '/start')
 async def cmd_start(message: Message):
     await message.answer("Привет", reply_markup=kb.main)
 
 
 @router.message(F.text == "/notification")
-async def notification(message: Message):
+async def notification(message: Message, state: FSMContext):
     user_id = message.from_user.id
     user_id = f"{user_id}"
     data = json_support.read_inf(push_data)
     if user_id not in data.keys():
         await message.answer("Хотите ли вы включить ежедневные уведомления?", reply_markup=kb.yes_no_keyboard)
+        await state.set_state(NotificationOrder.choosing_add_notification)
     else:
         await message.answer(f"Что хотите?\nВаше текущее время уведомления: {data[user_id]}",
                              reply_markup=kb.change_off_keyboard)
 
 
-@router.callback_query(F.data == "Time_add_yes")
-async def add_user(callback: CallbackQuery):
+@router.callback_query(NotificationOrder.choosing_add_notification, F.data == "Time_add_yes")
+async def add_user(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer("Напишите удобное для вас время")
-    user_id = callback.message.from_user.id
+    await state.set_state(NotificationOrder.time_add_notification_state)
+
+
+@router.message(NotificationOrder.time_add_notification_state)
+async def time_adding(message: Message):
+    user_id = message.from_user.id
     user_id = f"{user_id}"
     data = json_support.read_inf(push_data)
-    user_time = callback.message.text
+    user_time = message.text
     c = {user_id: user_time}
     data.update(c)
     json_support.write_inf(data, push_data)
-
+    await message.answer(f"Вы добавили уведомление на {user_time}.")
 
 @router.callback_query(F.data == "Time_add_no")
 async def answer_no(callback: CallbackQuery):
