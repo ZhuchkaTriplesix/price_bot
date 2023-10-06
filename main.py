64777a114d2e0c0d58e6297011d9869801729468
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-06 18:20:23 +0600

    fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ push_data = "users_time.json"
 async def main():
     with open("token.txt", "r") as TOKEN:
         bot_token = TOKEN.readline()
-    bot = Bot(bot_token)
+    bot = Bot("6407340014:AAFsR_mctp9ibyTYlKA7EuHoCBZamcXmw_k")
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot, skip_updates=True)
