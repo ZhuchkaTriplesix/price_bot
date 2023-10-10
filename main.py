c2021f283aabf336d1a8554d9ff6d3c75f52f9fe
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-10 10:19:52 +0600

    style(code restyle)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def main():
         bot_token = TOKEN.readline()
     bot = Bot(bot_token)
     dp = Dispatcher()
-    dp.include_routers(admin.router,vip.router, client.router)#routers add
+    dp.include_routers(admin.router, vip.router, client.router) #routers add
     await dp.start_polling(bot, skip_updates=True)
 
 
