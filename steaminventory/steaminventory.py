84bde277714975812675b3c8d862e55328d76852
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-17 15:59:11 +0600

    fix

--- steaminventory/steaminventory.py
+++ steaminventory/steaminventory.py
@@ -1,7 +1,6 @@
 import json
-
 import requests
-from urllib.request import urlopen
+
 
 def get_inventory(steamid):
     url = f'https://steamcommunity.com/inventory/{steamid}/730/2?l=russian&count=75'
