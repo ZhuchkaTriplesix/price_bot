c8f2a55ccf03d0cb83d35746cd3481ef270a1e85
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-19 15:37:01 +0600

    feat(vip_help)

--- handlers/vip.py
+++ handlers/vip.py
@@ -7,10 +7,19 @@ import keyboards as kb
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
 import steammarket as sm
+import models
 
 router = Router()
 
 
 @router.message(F.text == "/support")
 async def support(message: Message):
-    await message.answer("@ZhuchkaTriplesix")
+    if models.check_access(message.from_user.id) in ("Vip", "Admin", "Owner"):
+        await message.answer("Напишите - @ZhuchkaTriplesix")
+    else:
+        await message.answer("У вас нет доступа к этой команде")
+
+
+@router.message(F.text == "/vip_help")
+async def vip_help(message: Message):
+    await message.answer("Команды для Vip пользователей:\n/support - Персональная помощь с ботом")
