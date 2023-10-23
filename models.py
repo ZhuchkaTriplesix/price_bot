a8166bf166f1bf46f4967b9e263b8516fd105140
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-23 17:49:50 +0600

    delete unused imports

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-import time
 from sqlalchemy import Column, Integer, String, DateTime, BigInteger
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
