dcf4d1ffa7f3e01b437f235728ab02994887abee
JuntexZhuk <mrlololoshka94@gmail.com>
2023-10-23 17:11:02 +0600

    fix(change database models)

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import time
-from sqlalchemy import Column, Integer, String, DateTime, BigInteger, Float
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, Float, ForeignKey
 from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
 import datetime
@@ -16,22 +16,22 @@ session = scoped_session(Session)
 conn = engine.connect()
 
 
-class CasesPrice(Base):
-    __tablename__ = "prices"
-    time_check = Column(DateTime, default=datetime.datetime.utcnow, primary_key=True)
-    Clutch_Case = Column(Float)
-    Danger_Zone_Case = Column(Float)
-    CS20_Case = Column(Float)
-
-
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    nickname = Column(String)
     telegram_id = Column(BigInteger, index=True)
-    access = Column(String, default="User")
-    steam_id = Column(BigInteger, index=True, default=None)
-    created = Column(DateTime, default=datetime.datetime.utcnow)
+    username = Column(String(length=32))
+    group_id = Column(Integer, default=0)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class Items(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    hash_name = Column(String(length=30))
+    item_count = Column(Integer)
 
 
 Base.metadata.create_all(engine)
@@ -44,94 +44,79 @@ def add_close(user, session):
     print("Successful adding")
 
 
-def add_user(telegram_id, nickname):
-    session = Session()
-    user = session.query(Users).where(Users.telegram_id == telegram_id).first()
-    print(user)
-    if user is not None:
-        print("User is already in database")
-    else:
-        user = Users(nickname=nickname, telegram_id=telegram_id)
-        add_close(user, session)
-
-
-def add_admin(telegram_id, nickname):
+def add_user(telegram_id, username):
     session = Session()
     user = session.query(Users).where(Users.telegram_id == telegram_id).first()
     if user is not None:
         print("User is already in database")
     else:
-        user = Users(nickname=nickname, telegram_id=telegram_id, access="Admin")
+        user = Users(telegram_id=telegram_id, username=username)
         add_close(user, session)
 
 
-def change_access(telegram_id, access):
+def add_admin(telegram_id, username):
     session = Session()
     user = session.query(Users).where(Users.telegram_id == telegram_id).first()
     if user is not None:
-        user.access = access
+        print("User is already in database")
+    else:
+        user = Users(username=username, telegram_id=telegram_id, group_id=2)
+        add_close(user, session)
+
+
+def change_access(telegram_id, group_id):
+    session = Session()
+    user = session.query(Users).where(Users.telegram_id == telegram_id).first()
+    if user is not None:
+        user.group_id = group_id
+        user.updated_at = datetime.datetime.utcnow()
         session.commit()
         session.close()
 
 
-def check_access(telegram_id):
-    user = session.query(Users).filter(Users.access == "Admin").where(Users.telegram_id == telegram_id).first()
-    if user is not None:
-        return "Admin"
-    else:
-        user = session.query(Users).filter(Users.access == "User").where(Users.telegram_id == telegram_id).first()
-        if user is not None:
-            return "User"
-        else:
-            user = session.query(Users).filter(Users.access == "Owner").where(Users.telegram_id == telegram_id).first()
-            if user is not None:
-                return "Owner"
-            else:
-                user = session.query(Users).filter(Users.access == "Vip").where(
-                    Users.telegram_id == telegram_id).first()
-                if user is not None:
-                    return "Vip"
-                else:
-                    return None
-
-
-def change_steam_id(telegram_id, steam_id):
-    session = Session()
+def check_vip(telegram_id):
     user = session.query(Users).where(Users.telegram_id == telegram_id).first()
-    user.steam_id = steam_id
-    session.commit()
-    session.close()
-
-
-def get_steamid(telegram_id):
-    session = Session()
-    user = session.query(Users).where(Users.telegram_id == telegram_id).first()
-    if user.steam_id is None:
-        session.close()
-        return None
+    user_group = user.group_id
+    if user_group >= 1:
+        return True
     else:
-        user_message = f"{user.steam_id}"
-        session.close()
-        return user_message
+        return False
 
 
-def add_close_case(cases, session):
-    session.add(cases)
-    session.commit()
-    session.close()
+def check_admin(telegram_id):
+    user = session.query(Users).where(Users.telegram_id == telegram_id).first()
+    user_group = user.group_id
+    if user_group >= 2:
+        return True
+    else:
+        return False
 
 
-def get_prices(cases):
+def get_id(telegram_id):
+    user = session.query(Users).where(Users.telegram_id == telegram_id).first()
+    user_id = user.id
+    return user_id
+
+
+def add_item(telegram_id, hash_name, item_count):
     session = Session()
-    const_cases = cases
-    prices = []
-    for case in const_cases:
-        price = sm.get_item(730, case, currency='RUB')
-        price = price['lowest_price']
-        price = price[:-5]
-        price = price.replace(",", ".")
-        prices.append(float(price))
-    output = CasesPrice(Clutch_Case=prices[0], Danger_Zone_Case=prices[1], CS20_Case=prices[2])
-    add_close_case(output, session)
+    user_id = get_id(telegram_id)
+    item = session.query(Items).filter(Items.hash_name == hash_name).where(Items.user_id == user_id).first()
+    if item is None:
+        item = Items(user_id=user_id, hash_name=hash_name, item_count=item_count)
+        session.add(item)
+        session.commit()
+        session.close()
 
 
+def user_items(telegram_id):
+    session = Session()
+    user_id = get_id(telegram_id)
+    items = session.query(Items).where(Items.user_id == user_id).all()
+    user_item = {}
+    for i in items:
+        hash_name = i.hash_name
+        item_count = i.item_count
+        u = {hash_name: item_count}
+        user_item.update(u)
+    return user_item
