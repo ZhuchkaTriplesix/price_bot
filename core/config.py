3e15806c828e9e246d86c8e00254d3b397c9e07b
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2024-05-14 19:00:34 +0300

    refactor(modules rename)

---
+++ core/config.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict, Optional
+from dotenv import find_dotenv, load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, validator
+
+load_dotenv(find_dotenv(".env"))
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(case_sensitive=True)
+    TOKEN: str
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+pg8000",
+            username=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=values.get("POSTGRES_DB"),
+        )
+
+
+settings = Settings()
